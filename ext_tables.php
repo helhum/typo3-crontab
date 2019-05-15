2379fe0e9c49c96f3aa70517e6b0684574fb284a
Helmut Hummel <info@helhum.io>
2019-05-15 15:56:02 +0200

    [FEATURE] Hide scheduler module by default
    Scheduler extension is required to be able
    to execute legacy scheduler tasks, but the module
    itself is useless when crontab is used, so we hide it by default.
    
    It could be useful though during migration, so we make
    it possible to enable it by configuration.

--- ext_tables.php
+++ ext_tables.php
@@ -4,8 +4,8 @@
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
         'Helhum.TYPO3.Crontab',
         'system',
-        'tx_Crontab',
-        '',
+        'Module',
+        'after:BeuserTxBeuser',
         [
             'CrontabModule' => 'list, toggleSchedule, terminate, edit, delete, schedule',
         ],
@@ -15,4 +15,7 @@
             'labels' => 'LLL:EXT:crontab/Resources/Private/Language/locallang_mod.xlf'
         ]
     );
+    if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['crontab']['hideSchedulerModule'])) {
+        $GLOBALS['TBE_MODULES']['system'] = str_replace([',txschedulerM1', ',,'], ['',','], $GLOBALS['TBE_MODULES']['system']);
+    }
 })();
