6b1836f6d63e860bb41b33fa1f2b1b638bc4f228
Helmut Hummel <typo3@helhum.io>
2021-02-14 15:32:56 +0100

    Remove TYPO3 9.5 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => '',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '10.4.0-10.4.99',
         ],
         'conflicts' => [],
         'suggests' => [
